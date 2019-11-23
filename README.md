e8876166da1e44f1da999c6ad05b75be1b3f826b
baumannpa <baumannpa@gmail.com>
2019-11-23 20:10:25 +0100

    [update] refactoring to show different approaches to build network

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # PyTorch-MNIST
-Modified version of [PyTorch MNIST example](https://github.com/pytorch/examples/tree/master/mnist). This repository splits the code into parts (training, model and data) for a better overview and further adaptations.
+Simplified adaption of [PyTorch MNIST example](https://github.com/pytorch/examples/tree/master/mnist). This repository splits the code into parts (training, model and data) for a better overview and further adaptations.
 
 ## Requirements
 pip install -r requirements.txt
--- models/SimpleFC.py
+++ models/SimpleFC.py
@@ -1,4 +1,4 @@
-from torch.nn import Linear, Module
+from torch.nn import Linear, Module, ReLU, Sequential
 import torch.nn.functional as F
 
 
@@ -13,13 +13,16 @@ class SimpleFC(Module):
         self.width = input_shape[2]
 
         # network layers
-        self.layer_1 = Linear(in_features=self.height * self.width * self.depth, out_features=200)
-        self.layer_2 = Linear(in_features=200, out_features=200)
+        # variate 1
+        self.layer_1 = Sequential(
+            Linear(in_features=self.height * self.width * self.depth, out_features=200),
+            ReLU())
+        self.layer_2 = Linear(in_features=200, out_features=200)  # variate 2
         self.layer_3 = Linear(in_features=200, out_features=10)
 
     def forward(self, x):
         x = x.view(-1, self.height * self.width)
-        x = F.relu(self.layer_1(x))
-        x = F.relu(self.layer_2(x))
+        x = self.layer_1(x)  # variate 1
+        x = F.relu(self.layer_2(x))  # variate 2
         x = self.layer_3(x)
         return F.log_softmax(x, dim=1)
