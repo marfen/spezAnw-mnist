7328e70b4d0fe8a441cceb6fd8c679ebef839434
Patrick Baumann <baumann.pa@googlemail.com>
2019-11-24 13:11:15 +0000

    Update SimpleFC.py

--- models/SimpleFC.py
+++ models/SimpleFC.py
@@ -13,16 +13,16 @@ class SimpleFC(Module):
         self.width = input_shape[2]
 
         # network layers
-        # variate 1
+        # variat 1
         self.layer_1 = Sequential(
             Linear(in_features=self.height * self.width * self.depth, out_features=200),
             ReLU())
-        self.layer_2 = Linear(in_features=200, out_features=200)  # variate 2
+        self.layer_2 = Linear(in_features=200, out_features=200)  # variat 2
         self.layer_3 = Linear(in_features=200, out_features=10)
 
     def forward(self, x):
         x = x.view(-1, self.height * self.width)
-        x = self.layer_1(x)  # variate 1
-        x = F.relu(self.layer_2(x))  # variate 2
+        x = self.layer_1(x)  # variat 1
+        x = F.relu(self.layer_2(x))  # variat 2
         x = self.layer_3(x)
         return F.log_softmax(x, dim=1)
