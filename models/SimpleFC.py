3e8546f2c45e55d38e67a028fa87232c0baac3b6
baumannpa <baumannpa@gmail.com>
2020-03-31 09:57:13 +0200

    [update] changed depth to channels for clarification

--- models/SimpleFC.py
+++ models/SimpleFC.py
@@ -7,15 +7,16 @@ class SimpleFC(Module):
         super(SimpleFC, self).__init__()
 
         # split input according to your network's design,
-        # in this case a single incoming image split into its height, width and depth
-        self.depth = input_shape[0]
+        # in this case a single incoming image split into its height, width and channels
+        # i.e. for original mnist (mono8) it is 28x28x1, for a rgb version it would be 28x28x3
+        self.channels = input_shape[0]
         self.height = input_shape[1]
         self.width = input_shape[2]
 
         # network layers
         # variant 1
         self.layer_1 = Sequential(
-            Linear(in_features=self.height * self.width * self.depth, out_features=200),
+            Linear(in_features=self.height * self.width * self.channels, out_features=200),
             ReLU())
         self.layer_2 = Linear(in_features=200, out_features=200)  # variant 2
         self.layer_3 = Linear(in_features=200, out_features=10)
