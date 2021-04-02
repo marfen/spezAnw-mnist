5c9128507d08f82ef2833eb653b66830c2f2ee89
marfen <fenzl.mario94@gmail.com>
2021-04-02 17:56:42 +0200

    initial upload

--- README.md
+++ README.md
@@ -1,8 +1,1 @@
-# PyTorch-MNIST
-Simplified adaption of [PyTorch MNIST example](https://github.com/pytorch/examples/tree/master/mnist). This repository splits the code into parts (training, model and data) for a better overview and further adaptations.
-
-## Requirements
-pip install -r requirements.txt
-
-## Run
-python main.py
+Code zu [Task 3](https://github.com/marfen/spezielle_anwendungen_ros-ai)
--- main.py
+++ main.py
@@ -1,88 +1,76 @@
 from __future__ import print_function
-
 from os import mkdir
-from multiprocessing import cpu_count
-
+from torchvision import datasets, transforms
 import torch as t
-
-from data import mnist_loader
-from models.SimpleFC import SimpleFC
-
-# select number of cpu workers, maximum of 8
-MAX_NB_CPU = 8
-NB_CPU = cpu_count() if cpu_count() < MAX_NB_CPU else MAX_NB_CPU
-
-# use cuda if available
-use_cuda = t.cuda.is_available()
-device = t.device("cuda" if use_cuda else "cpu")
-kwargs = {'num_workers': NB_CPU, 'pin_memory': True} if use_cuda else {}
+from models.Model1 import Model1
+from models.Model2 import Model2
 
 
-def train(train_loader, model, optimizer, criterion, epoch, log_interval):
-    model.train()
-    for batch_idx, (data, target) in enumerate(train_loader):
-        data, target = data.to(device), target.to(device)
+kwargs = {'num_workers': 1, 'pin_memory': True}
+
+def train(traingsdata, model, epoch):
+    model.train() #weights changing over time due to training
+    for batch_idx, (data, target) in enumerate(traingsdata):
+        optimizer = t.optim.SGD(model.parameters(), lr=0.1)
         optimizer.zero_grad()
-        output = model(data)
-        loss = criterion(output, target)
+        out = model(data)
+        criterion = t.nn.NLLLoss()
+        loss = criterion(out, target)
         loss.backward()
         optimizer.step()
-        if batch_idx % log_interval == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset), 100. * batch_idx / len(train_loader),
-                loss.item()))
+
+        print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+            epoch, batch_idx * len(data), len(traingsdata.dataset), 100. * batch_idx / len(traingsdata),
+            loss.item()))
 
 
-def test(test_loader, model, criterion):
-    model.eval()
+def test(testdata, model):
+    model.eval()  #fixed weights for evaluation
     test_loss = 0
     correct = 0
     with t.no_grad():
-        for data, target in test_loader:
-            data, target = data.to(device), target.to(device)
+        for data, target in testdata:
             output = model(data)
-            test_loss += criterion(output, target).item()  # sum up batch loss
-            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            criterion = t.nn.NLLLoss()
+            test_loss += criterion(output, target).item()
+            pred = output.argmax(dim=1, keepdim=True)
             correct += pred.eq(target.view_as(pred)).sum().item()
 
-    test_loss /= len(test_loader.dataset)
+    test_loss /= len(testdata.dataset)
 
     print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-        test_loss, correct, len(test_loader.dataset), 100. * correct / len(test_loader.dataset)))
+        test_loss, correct, len(testdata.dataset), 100. * correct / len(testdata.dataset)))
 
 
 def main():
-    # training parameters
-    batch_size = 200
-    epochs = 10
-    learning_rate = 0.01
-    log_interval = 10
-    save_model = True
 
-    # create neural network models
-    model = SimpleFC(input_shape=[1, 28, 28]).to(device)
+    #(un)comment preferred model
+    # sigmoid activation function
+    model = Model1(input_shape=[])
 
-    # create a stochastic gradient descent optimizer
-    optimizer = t.optim.SGD(model.parameters(), lr=learning_rate)
-    # create a loss function
-    criterion = t.nn.NLLLoss()
+    #relu activation function
+    #model = Model2(input_shape=[])
 
-    # get training and test data sets
-    train_loader = mnist_loader.get_train_loader(batch_size=batch_size, kwargs=kwargs)
-    test_loader = mnist_loader.get_test_loader(batch_size=batch_size, kwargs=kwargs)
 
-    # train and test for x epochs
-    for epoch in range(1, epochs + 1):
-        train(train_loader, model, optimizer, criterion, epoch, log_interval)
-        test(test_loader, model, criterion)
+    # Aus Kursfolien übernommen
+    trainingsdata = t.utils.data.DataLoader(datasets.MNIST('mnist_data_sets', train=True, download=True,
+                    transform=transforms.Compose([transforms.ToTensor(),transforms.Normalize((0.1307,), (0.3081,))])),
+                    batch_size=64, shuffle=True, **kwargs)
 
-    if save_model:
-        try:
-            mkdir("trained_models")
-        except FileExistsError:
-            pass
-        t.save({'state_dict': model.state_dict()}, 'trained_models/mnist_simple_fc.pth')
+    testdata = t.utils.data.DataLoader(datasets.MNIST('mnist_data_sets', train=False, download=True,
+                    transform=transforms.Compose([transforms.ToTensor(),transforms.Normalize((0.1307,), (0.3081,))])),
+                    batch_size=64, shuffle=True, **kwargs)
 
+
+    for epoch in range(1, 6): # 5 epochen
+        train(trainingsdata, model, epoch)
+        test(testdata, model)
+
+
+
+    #(un)comment preferred model
+    t.save(model.state_dict(), 'persistent_models/Model1_sigmoid.pth')
+    #t.save(model.state_dict(), 'persistent_models/Model2_relu.pth')
 
 if __name__ == '__main__':
     main()
